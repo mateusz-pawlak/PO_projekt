2a2bc77123d0a06260206450966c2c636d140e09
Mateusz Pawlak <mateuszpawlak1001@gmail.com>
2021-04-22 21:08:17 +0200

    JavaFX

---
+++ HW-Info/src/main/java/pl/bieniek/sendInfoController.java
@@ -0,0 +1,19 @@
+package pl.bieniek;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+
+public class sendInfoController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+}
