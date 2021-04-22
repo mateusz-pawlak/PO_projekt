a13120753a861898e1e005360f4a2d6668bd2f02
Mateusz Pawlak <mateuszpawlak1001@gmail.com>
2021-04-22 21:05:03 +0200

    JavaFX

--- HW-Info/src/main/java/pl/bieniek/Main.java
+++ HW-Info/src/main/java/pl/bieniek/Main.java
@@ -1,9 +1,24 @@
 package pl.bieniek;
 
-import java.util.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sendInfo.fxml"));
+        primaryStage.setTitle("Client info");
+        primaryStage.setScene(new Scene(root, 700, 400));
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
+        launch(args);
         SYSTEM os = new SYSTEM();
         System.out.println("----------OS-----------");
         System.out.println(os.manufacturer+" "+os.family+" "+ os.ver);
@@ -19,5 +34,4 @@ public class Main {
         System.out.println("----------RAM----------");
         System.out.println("Ilość RAMu: "+ram1.total+" GB");
     }
-
 }
