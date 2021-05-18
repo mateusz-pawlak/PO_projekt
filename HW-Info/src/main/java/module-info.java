48bd76d5291d88d167fa13c9aa77cbf495807895
Rafal <rafal.tadaszak@pwr.edu.pl>
2021-05-18 23:49:31 +0200

    Working

--- HW-Info/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module HWInfo {
-    requires javafx.controls;
-    requires javafx.graphics;
-    requires javafx.fxml;
-    requires com.github.oshi;
-    requires java.sql;
-    requires mysql.connector.java;
-    opens pl.bieniek ;
-}
